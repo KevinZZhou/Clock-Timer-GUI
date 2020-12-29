f1a421ceb526aab5d4df5d12d0e49a34a65d3476
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2020-12-29 17:51:42 -0500

    Update DigitalClock.py
    Added functionality to the timezone dropdown box
    
    TODO:
    Alarms?
    Move onto next mode?

--- DigitalClock.py
+++ DigitalClock.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from datetime import datetime
 from tkinter import ttk
 from time import strftime
 from dateutil.zoneinfo import get_zonefile_instance
+from dateutil.tz import gettz
 from tzlocal import get_localzone
 
 class DigitalClock(tk.Frame):
@@ -30,7 +32,9 @@ class DigitalClock(tk.Frame):
         # Create the time label
         self.label = tk.Label(root, text = self.time, font = ("arial", 
                 32, "bold"), background = "navy", foreground = "white")
-        
+        # Create the dropdown box label
+        self.dropdown_label = tk.Label(root, text = "Select a timezone:", 
+                font = ("arial", 12))
         # Create the timezone dropdown box
         timezone_list = list(get_zonefile_instance().zones)
         timezone_list.sort()
@@ -46,6 +50,7 @@ class DigitalClock(tk.Frame):
         
         # Place widgets into frame
         self.label.pack(anchor = "center", expand = True, fill ="both")
+        self.dropdown_label.pack()
         self.timezone_dropdown.pack()
 
         # Start the clock
@@ -53,10 +58,20 @@ class DigitalClock(tk.Frame):
 
     def tick(self):
         """
-        This function updates the displayed time.
+        This function updates the displayed time based on the timezone.
         """
 
-        time: str = strftime("%H:%M:%S %p")
-        self.time = time
-        self.label.config(text = self.time)
-        self.label.after(1000, self.tick)
+        # Use the timezone dropdown box to adjust the displayed time
+        # If something goes wrong, default to displaying local time
+        try:
+            current_timezone: str = self.timezone_dropdown.get()
+            timezone_object = gettz(current_timezone)
+            datetime_object = datetime.now(tz = timezone_object)
+            time: str = datetime_object.strftime("%I:%M:%S %p %Z")
+        except:
+            time: str = strftime("%I:%M:%S %p %Z")
+
+        if self.time != time:
+            self.time = time
+            self.label.config(text = self.time)
+        self.label.after(100, self.tick)
