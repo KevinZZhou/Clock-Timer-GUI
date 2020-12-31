b3076b216d3145902af60677f4c28c8522962626
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2020-12-30 19:51:15 -0500

    Update AnalogClock.py
    Add in a date label and timezone dropdown box into AnalogClock.py
    Add basic functionality for tick() to allow for the date label to update
    
    TODO:
    Time functionality by showing moving clock hands

--- AnalogClock.py
+++ AnalogClock.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from pathlib import Path
+from time import strftime
+
+from TimezoneDropdown import TimezoneDropdown
 
 class AnalogClock(tk.Frame):
     """
@@ -25,6 +28,8 @@ class AnalogClock(tk.Frame):
         self.root = root
         self.canvas = tk.Canvas(self.root, width = 600, height = 600, 
                 bg = "white")
+        self.time = ""
+        self.date = ""
 
         # Place the analog clock image on the canvas
         path = Path("files/images/")
@@ -32,10 +37,35 @@ class AnalogClock(tk.Frame):
         self.image = tk.PhotoImage(file = analog_clock)
         self.canvas.create_image(300, 300, image = self.image)
 
+        # Create the date label and timezone dropdown box
+        self.date_label = tk.Label(root, text = self.date, 
+                font = ("verdana", 16, "bold"), background = "white", 
+                foreground = "black", borderwidth = 1, relief = "solid")
+        self.dropdown = TimezoneDropdown(root)
+
         # Place widgets into frame
         self.canvas.pack()
+        self.date_label.pack(expand = True, fill ="both")
+        self.dropdown.pack()
+
+        # Start the clock
+        self.tick()
 
     def tick(self):
         """
         This function updates the displayed time based on the timezone.
         """
+
+        # Use the timezone dropdown box to adjust the displayed date
+        try:
+            datetime_object = self.dropdown.get_datetime()
+            date: str = datetime_object.strftime("%A, %B %d, %Y")
+        except:
+            date: str = strftime("%A, %B %d, %Y")
+        
+        # Update date if necessary
+        if self.date != date:
+            self.date = date
+            self.date_label.config(text = self.date)
+        
+        self.after(100, self.tick)
